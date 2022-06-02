01655846bf33ed66f21815f89f28a17b2fc120e5
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-02 19:48:49 +0300

    Add files via upload

---
+++ Algorithms/Implementations/HalloweenSale.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnswerToHackerRankHallowenSale
+{
+    class Program
+    {
+
+        public static int howManyGames(int p, int d, int m, int s)
+        {
+            int answer = 0;
+
+            while (s - p >= 0)
+            {
+                s -= p;
+                p -= d;
+                if (m >= p)
+                {
+                    p = m;
+                }
+                answer++;
+            }
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter initial price:");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the discount price:");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the last price");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the budget");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            int result = howManyGames(p, d, m, s);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
