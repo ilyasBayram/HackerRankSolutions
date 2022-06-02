92613b81e5ef71f1126a52e0c36b9ea59c567524
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-02 17:25:22 +0300

    Add files via upload

---
+++ Algorithms/Implementations/ViralAdvertising.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ViralAdvertising
+{
+    class Program
+    {
+
+        public static int viralAdvertising(int n)
+        {
+            int shared = 5;
+            int cumulative = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liked = (shared / 2);
+                shared = liked * 3;
+                cumulative += liked;
+            }
+            return cumulative;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please ente the The day number: ");
+            int n = int.Parse(Console.ReadLine());
+            int answer = viralAdvertising(n);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
