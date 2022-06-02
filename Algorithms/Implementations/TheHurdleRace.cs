ad386ac280c221c8905b04e5c2e4deece96b7e73
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-02 17:18:51 +0300

    Add files via upload

---
+++ Algorithms/Implementations/TheHurdleRace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheHurdleRace
+{
+    class Program
+    {
+        public static int hurdleRace(int k, List<int> height)
+        {
+            return k > height.Max() ? 0 : height.Max()-k;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> height = new List<int>();
+
+            Console.Write("\nPlease enter the hurdles number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("\nPlse enther the jump height: ");
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\nPlease enter the height of {0}. hurdle : ", i+1);
+                height.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int result= hurdleRace(k, height);
+            Console.WriteLine("\n"+result);
+            Console.ReadLine();
+        }
+    }
+}
