7bf500efc5fb72fe58fbf431145d82a8a6bfbbd0
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-02 17:23:37 +0300

    Add files via upload

---
+++ Algorithms/Implementations/UtopianTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UtopianTree
+{
+    class Program
+    {
+        public static int utopianTree(int n)
+        {
+
+            int height = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                height = i % 2 == 0? height += 1: height *= 2;
+            }
+            return height;
+
+        }
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
