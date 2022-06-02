3daa1d6adad0bd85bd735131047128e5fc3f1eac
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-02 16:51:25 +0300

    Add files via upload

---
+++ Algorithms/Implementations/DrawingBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DrawingBook
+{
+    class Program
+    {
+
+        public static int pageCount(int n, int p)
+        {
+            
+            int distanceFromPageToOne;
+
+            int distanceFromEndToPage;
+
+            distanceFromPageToOne = p / 2;
+
+            distanceFromEndToPage = n / 2 - p / 2;
+            
+            if (distanceFromPageToOne < distanceFromEndToPage)
+
+            return distanceFromPageToOne;
+
+            return distanceFromEndToPage;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter how many pages the book is?: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter whic number pages you want to go to?: ");
+
+            int p = int.Parse(Console.ReadLine());
+
+            int result = pageCount(n, p);
+
+            Console.WriteLine("\nThe asnwer is {0}.", result);
+
+            Console.ReadLine();
+        }
+    }
+}
