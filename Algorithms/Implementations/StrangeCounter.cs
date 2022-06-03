f87071d86f1c07b8a48cba358896c3afaf7d3b6d
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-03 11:33:46 +0300

    Add files via upload

---
+++ Algorithms/Implementations/StrangeCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StrangeCounter
+{
+    class Program
+    {
+        
+        public static long strangeCounter(long t)
+        {
+
+            int count = 3;
+            int temp = 3;
+            while (temp < t)
+            {
+                count *= 2;
+                temp += count;
+            }
+            return temp - t + 1;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the time: ");
+            long t = long.Parse(Console.ReadLine());
+            long result = strangeCounter(t);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
