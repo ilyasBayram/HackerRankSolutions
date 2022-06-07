02a2059e01d00c390c3b7b248f68cdb035af930f
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-07 13:21:52 +0300

    Add files via upload

---
+++ Algorithms/String/camelCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace camelCase
+{
+    class Program
+    {
+        public static int camelcase(string s)
+        {
+            int count = 0;
+
+            foreach(char c in s)
+            {
+                if (char.IsUpper(c))
+                {
+                    count++;
+                }
+            }
+
+            return count + 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the word: ");
+            string s = Console.ReadLine();
+            int result = camelcase(s);
+            Console.WriteLine("\n"+result);
+            Console.ReadLine();
+
+        }
+    }
+}
