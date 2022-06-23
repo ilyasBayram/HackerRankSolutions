3b00555d1c3fe002f690febe11b9d4630bc2b4df
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-23 10:38:01 +0300

    Add files via upload

---
+++ Algorithms/Implementations/ChocolateFeast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChocolateFeast
+{
+    class Program
+    {
+
+        public static int chocolateFeast(int n, int c, int m)
+        {
+            int count = n / c;
+            int wrapper = n / c;
+
+            for (int i = 0; wrapper >= m; i++)
+            {
+                wrapper = wrapper - (m + 1);
+                count++;
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 12;
+            int c = 4;
+            int m = 4;
+
+            int answer = chocolateFeast(n, c, m);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
