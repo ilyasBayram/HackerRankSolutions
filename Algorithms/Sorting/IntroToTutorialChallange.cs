4172487fd429d443aa684e54c30af83d95e0afcc
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-23 18:59:45 +0300

    Add files via upload

---
+++ Algorithms/Sorting/IntroToTutorialChallange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroToTutorialChallange
+{
+    class Program
+    {
+        // you can use "indexOf()" method either.
+        // return arr.indexOf(V);
+        public static int introTutorial(int V, List<int> arr)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] == V)
+                {
+                    answer = i;
+                }
+            }
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>() { 1, 4, 5, 7, 9, 12 };
+            int V = 4;
+            int result = introTutorial(V, arr);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
