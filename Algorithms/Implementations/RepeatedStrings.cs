b48a980227e303398e71fd3ac898b1051af5527d
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-28 13:35:16 +0300

    Add files via upload

---
+++ Algorithms/Implementations/RepeatedStrings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RepeatedString
+{
+    class Program
+    {
+        public static long repeatedString(string s, long n)
+        {
+            long count = 0;
+
+            if (s.Length > 1)
+            {
+                for (int i = 0; i < s.Length; i++)
+                    if (s[i] == 'a')
+                        count++;
+
+                count *= n / s.Length;
+
+                for (int j = 0; j < n % s.Length; j++)
+                {
+                    if (s[j] == 'a')
+                        count++;
+
+                }
+
+            }
+            else
+            {
+                if (!s.Contains('a'))
+                    count = 0;
+                else
+                    count = n;
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 10;
+            string s = "aba";
+
+           long result = repeatedString(s, n);
+         
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
