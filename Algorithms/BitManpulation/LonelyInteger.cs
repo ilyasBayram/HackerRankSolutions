deb8b187d0d0d87fbef4a2dd2975c4d59af796c9
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-28 19:28:32 +0300

    Add files via upload

---
+++ Algorithms/BitManpulation/LonelyInteger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LonelyInteger
+{
+    class Program
+    {
+
+        public static int lonelyinteger(List<int> a)
+        {
+            int count = 0;
+
+            for (int i = 0; i < a.Count - 1; i++)
+            {
+                for (int j = i + 1; j < a.Count ; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        count++;
+                        a.RemoveAt(i);
+                        a.RemoveAt(j-1);
+                        i = -1;
+                        break;
+                    }
+
+                }
+
+            }
+
+            return a[0];
+        }
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 0, 0, 1, 2, 1 };
+
+            int result = lonelyinteger(a);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
