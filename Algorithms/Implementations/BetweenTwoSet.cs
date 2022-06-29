c979d7228074e128a03b04e1d9aa76ecf58e6223
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-29 16:35:21 +0300

    Add files via upload

---
+++ Algorithms/Implementations/BetweenTwoSet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BetweenToSet
+{
+    class Program
+    {
+
+        public static int getTotalX(List<int> a, List<int> b)
+        {
+            List<int> temp = new List<int>();
+
+            for (int i = 1; a[a.Count - 1] * i <= b[0]; i++)
+            {
+                temp.Add(a[a.Count-1]*i);
+                
+            }
+
+            for (int i = 0; i < a.Count-1; i++)
+            {
+                for (int j= 0; j < temp.Count; j++)
+                {
+                    if (temp[j] % a[i] != 0)
+                        temp.RemoveAt(i);
+                }
+                
+            }
+
+            for (int i = 0; i < temp.Count; i++)
+            {
+                for (int j = 0; j < b.Count; j++)
+                {
+                    if (b[j] % temp[i] != 0)
+                    {
+                        temp.RemoveAt(i);
+                        i -= 2;
+                        break;
+                    }
+                       
+                }
+                
+            }
+
+            return temp.Count;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 2};
+            List<int> b = new List<int>() { 20, 30, 12};
+
+            int result = getTotalX(a, b);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
