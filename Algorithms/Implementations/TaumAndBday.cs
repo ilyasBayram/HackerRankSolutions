ab4534052516770854679362f4e1fdfa9fe7c9ab
ilyasBayram <101389320+ilyasBayram@users.noreply.github.com>
2022-06-30 12:06:08 +0300

    Add files via upload

---
+++ Algorithms/Implementations/TaumAndBday.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaumAndBday
+{
+    class Program
+    {
+
+        public static long taumBday(int b, int w, int bc, int wc, int z)
+        {
+            return (long)b * Math.Min(bc, wc + z) + (long)w * Math.Min(wc, bc + z);
+        }
+        static void Main(string[] args)
+        {
+            int b = 27984;
+            int w = 1402;
+            int bc = 619246;
+            int wc = 615589;
+            int z = 247954;
+
+            long answer = taumBday(b, w, bc, wc, z);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+        }
+    }
+}
